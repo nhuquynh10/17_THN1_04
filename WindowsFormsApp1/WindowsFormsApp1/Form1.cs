392c57cbaf8029edc6af5d8fc6a331ed215dba51
nhuquynh10 <toannguyen.programmer@gmail.com>
2021-03-19 10:51:05 +0700

    Update Form1.cs

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -3,19 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+       
         Một mùa thu trước, mỗi hoàng hôn,
         Nhặt cánh hoa rơi chẳng thấy buồn,
         Nhuộm ánh nắng tà qua mái tóc,
